68c615ec64b86bfdf5de6aa017ed5e16e2d23cb4
Marcos Hilario <mrhilario1985@gmail.com>
2020-03-02 17:30:43 -0300

    Atualizando Banco de Dados

--- Banco de Dados/InLock_BD_DDL_01.sql
+++ Banco de Dados/InLock_BD_DDL_01.sql
@@ -1,4 +1,4 @@
- CREATE DATABASE InLock_Games_Tarde; USE InLock_Games_Tarde; CREATE TABLE Estudio (	IdEstudio		INT PRIMARY KEY IDENTITY,	NomeEstudio		VARCHAR(100) NOT NULL UNIQUE ); CREATE TABLE Jogo (	IdJogo			INT PRIMARY KEY IDENTITY,	NomeJogo		VARCHAR(100),	Descricao		VARCHAR(200),	DataLancamento	DATE, 	Valor			MONEY,	IdEstudio		INT FOREIGN KEY REFERENCES Estudio(IdEstudio)); CREATE TABLE TipoUsuario (
+ CREATE DATABASE InLock_Games_Tarde; USE InLock_Games_Tarde; CREATE TABLE Estudio (	IdEstudio		INT PRIMARY KEY IDENTITY,	NomeEstudio		VARCHAR(100) NOT NULL UNIQUE ); CREATE TABLE Jogo (	IdJogo			INT PRIMARY KEY IDENTITY,	NomeJogo		VARCHAR(100),	Descricao		VARCHAR(200),	DataLancamento	DATE, 	Valor			DOUBLE,	IdEstudio		INT FOREIGN KEY REFERENCES Estudio(IdEstudio)); CREATE TABLE TipoUsuario (
 	IdTipoUsuario	INT PRIMARY KEY IDENTITY,
 	Titulo			VARCHAR(100) NOT NULL UNIQUE
 );
@@ -8,4 +8,4 @@ CREATE TABLE Usuario (
 	Email			VARCHAR(100) NOT NULL UNIQUE,
 	Senha			VARCHAR(100) NOT NULL,
 	IdTipoUsuario	INT FOREIGN KEY REFERENCES TipoUsuario(IdTipoUsuario)
-);
+);select * from jogo
--- Banco de Dados/InLock_BD_DML_02.sql
+++ Banco de Dados/InLock_BD_DML_02.sql
@@ -4,6 +4,14 @@
  VALUES		('Blizzard'), ('Rockstar Studios'), ('Square Enix');
 
 INSERT INTO Jogo (NomeJogo, Descricao, DataLancamento, Valor, IdEstudio)
-VALUES		('Diablo 3', ' um jogo que contém bastante ação e é viciante, seja você um novato ou um fã', '20120515', '99', 1),
-			(' Red Dead Redemption II', 'jogo eletrônico de ação-aventura western', '20181026', '120', 2);
+VALUES		('Diablo 3', ' um jogo que contém bastante ação e é viciante, seja você um novato ou um fã', '20120515', 99, 1),
+			(' Red Dead Redemption II', 'jogo eletrônico de ação-aventura western', '20181026', 120, 2);
 
+INSERT INTO TipoUsuario (Titulo)
+VALUES ('Administrador'), ('Cliente');
+
+INSERT INTO Usuario (Email, Senha, IdTipoUsuario)
+VALUES		('admin@admin.com', 'admin', 1),
+			('cliente@cliente.com', 'cliente', 2);
+
+			
