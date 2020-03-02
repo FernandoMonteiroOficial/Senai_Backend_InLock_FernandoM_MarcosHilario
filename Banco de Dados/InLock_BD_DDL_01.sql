cf44fc64962ad677a54b392128eb12ed708b4855
Marcos Hilario <mrhilario1985@gmail.com>
2020-03-02 16:08:09 -0300

    First Commit

---
+++ Banco de Dados/InLock_BD_DDL_01.sql
@@ -0,0 +1,6 @@
+ CREATE DATABASE InLock_Games_Tarde; USE InLock_Games_Tarde; CREATE TABLE Estudio (	IdEstudio		INT PRIMARY KEY IDENTITY,	NomeEstudio		VARCHAR(100) ); Criar uma tabela de estúdios com os campos de IdEstudio e NomeEstudio;
+Criar uma tabela de jogos com os campos IdJogo, NomeJogo, Descricao,
+DataLancamento, Valor e IdEstudio;
+Criar uma tabela de tipos de usuários contendo os campos IdTipoUsuario e Titulo;
+Criar uma tabela de usuários contendo os campos de IdUsuario, Email, Senha e
+IdTipoUsuario;
