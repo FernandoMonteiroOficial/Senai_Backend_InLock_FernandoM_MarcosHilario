e72c9eaaae87c90ec3b2f6751e5bf5b97722e17e
Marcos Hilario <mrhilario1985@gmail.com>
2020-03-03 17:23:12 -0300

    Inserindo API Usuario

---
+++ backend/Senai.InLock.WebApi/Senai.InLock.WebApi/Interfaces/IUsuarioRepository.cs
@@ -0,0 +1,26 @@
+﻿using Senai.InLock.WebApi.Domains;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Senai.InLock.WebApi.Interfaces
+{
+    interface IUsuarioRepository
+    {
+
+        List<UsuarioDomain> Listar();
+
+        
+        UsuarioDomain BuscarPorId(int id);
+
+       
+       // void Cadastrar(UsuarioDomain novoUsuario);
+
+      
+       // void Deletar(int id);
+
+        
+       // void Atualizar(int id, UsuarioDomain UsuarioAtualizado);
+    }
+}
---
+++ backend/Senai.InLock.WebApi/Senai.InLock.WebApi/Repositories/UsuarioRepository.cs
@@ -0,0 +1,69 @@
+﻿
+using Senai.InLock.WebApi.Domains;
+using Senai.InLock.WebApi.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Senai.InLock.WebApi.Repositories
+{
+    public class UsuarioRepository : IUsuarioRepository
+    {
+
+        private string stringConexao = "Data Source=LAPTOP-6GS7ET34; initial catalog=InLock_Games_Tarde; user Id=sa; pwd=123456";
+
+        public UsuarioDomain BuscarPorId(int id)
+        {
+            
+        }
+
+        public List<UsuarioDomain> Listar()
+        {
+            List<UsuarioDomain> usuarios = new List<UsuarioDomain>();
+
+            using(SqlConnection con = new SqlConnection (stringConexao))
+            {
+                string querySelectAll = " SELECT U.IdUsuario, U.Email, U.IdTipoUsuario, TU.Titulo FROM Usuario U INNER JOIN TipoUsuario TU ON U.IdTipoUsuario = TU.IdTipoUsuario";
+                
+
+                con.Open();
+
+                SqlDataReader rdr;
+
+                using (SqlCommand cmd = new SqlCommand(querySelectAll, con))
+                {
+                    rdr = cmd.ExecuteReader();
+
+                    while (rdr.Read())
+                    {
+                        
+                        UsuarioDomain usuario = new UsuarioDomain
+                        {
+                            
+                            IdUsuario = Convert.ToInt32(rdr["IdUsuario"]),
+                            
+                            Email = rdr["Email"].ToString(),
+                            
+                            IdTipoUsuario = Convert.ToInt32(rdr["IdTipoUsuario"]),
+
+                            TipoUser = new TipoUsuarioDomain
+                            {
+                                IdTipoUsuario = Convert.ToInt32(rdr["IdTipoUsuario"]),
+                                
+                                Titulo = rdr["Titulo"].ToString()
+                            }
+                        };
+
+                        
+                        usuarios.Add(usuario);
+                    }
+
+                }
+            }
+
+            return usuarios;
+        }
+    }
+}
